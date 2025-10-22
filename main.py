9cc23cf4d558a0749c59774ba1ca81f5285cfe29
ElprogramadorxX <mdarruda@gmail.com>
2025-10-22 01:03:22 -0300

    Adicionando os pycache

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from scanner import scanner
-from ll1_parser import LL1Parser
-from grammar import grammar
+from scanner import analisador_lexico 
+from ll1_parser import AnalisadorSintaticoLL1
+from grammar import grammar, first, follow
 
 if __name__ == "__main__":
     with open("app.br", "r", encoding="utf-8") as f:
         codigo = f.read()
 
     print("\n=== ANÁLISE LÉXICA ===")
-    tokens = scanner(codigo)
+    tokens = analisador_lexico(codigo) 
     for t in tokens:
         print(t)
 
     print("\n=== ANÁLISE SINTÁTICA ===")
-    parser = LL1Parser(grammar)
+    parser = AnalisadorSintaticoLL1(grammar)
     parser.parse(tokens)
